cd9f0e36de37015b6c0c3f1bd79fd00a7e5791e9
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2021-02-26 20:26:31 +0530

    Moved funds from first user to ethSwap contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
 const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function (deployer) {
-    deployer.deploy(Token);
+module.exports = async function (deployer) {
+    // Deploy Token
+    await deployer.deploy(Token);
+    const token = await Token.deployed()
 
-    deployer.deploy(EthSwap);
+    // Deploy EthSwap
+    await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed()
+
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
 };
