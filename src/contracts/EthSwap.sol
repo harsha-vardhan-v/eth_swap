47d851be5f4832ac203f707013d0200656e64434
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2021-02-27 21:01:27 +0530

    Added buyTokens function and tested

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,6 +1,23 @@
 pragma solidity >=0.4.17;
 
+import "./Token.sol";
+
 contract EthSwap{
     string public name = "EthSwap Instant Exchange";
+
+    //Object for Token Smart Contract
+    Token public token;
+    uint public rate = 100;
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        //Redemption rate = # of tokens to swap for 1 ether
+        //Amount of Ethereum * Redemption Rate
+        uint tokenAmount = rate * msg.value;
+        token.transfer(msg.sender, tokenAmount);
+    }
 }
 
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -14,12 +14,12 @@ function tokens(n){
     return web3.utils.toWei(n, 'ether');
 }
 
-contract('EthSwap', (accounts) => {
+contract('EthSwap', ([deployer, investor]) => {
     let token, ethSwap
 
     before(async () => {
         token = await Token.new()
-        ethSwap = await EthSwap.new()
+        ethSwap = await EthSwap.new(token.address)
         //Transfer all 1 million tokens to EthSwap
         await token.transfer(ethSwap.address, tokens('1000000'))
     })
@@ -41,6 +41,33 @@ contract('EthSwap', (accounts) => {
             let balance = await token.balanceOf(ethSwap.address);
             assert.equal(balance, tokens('1000000'));
             
+        })
+    })
+
+    describe('buyTokens()', async () => {
+        let result
+
+        before(async () => {
+            //Purchase 1 ether from investor account
+            result = await ethSwap.buyTokens({ from: investor, value: web3.utils.toWei('1', 'ether') })
+            
+        })
+
+        it('Allows user to instantly purchase tokens from ethSwap fo fixed rate', async () =>{
+            //Check ivestor token balance after purchase
+
+            let investorBalance = await token.balanceOf(investor)
+
+            //Check investor token balance after purchase
+            assert.equal(investorBalance.toString(), tokens('100'))
+
+            //Check ethSwap token balance after purchase
+            let ethSwapBalance = await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), tokens('999900'))
+
+            //Check ethSwap ethereum balance after purchase
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), web3.utils.toWei('1', 'ether'))
         })
     })
 })
